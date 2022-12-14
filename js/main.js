9c53473c45db9223e8538efb3b1f4271747e3233
rizkirifandi7 <rizki.10121284@mahasiswa.ac.id>
2022-12-14 22:49:02 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 700, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+const navbar = document.querySelector('.navbar')
+
+window.addEventListener('scroll', () =>{
+  if(this.scrollY > 0){
+    navbar.classList.remove('py-3')
+    navbar.classList.add('shadow', 'py-2')
+  } else{
+    navbar.classList.add('py-3')
+    navbar.classList.remove('shadow', 'py-2')
+  }
+})
