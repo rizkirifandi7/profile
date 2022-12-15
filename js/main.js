70c86aeb7a075c5b23a508d43292a77700128eb5
rizkirifandi7 <rizki.10121284@mahasiswa.ac.id>
2022-12-15 13:46:57 +0700

    fix contact

--- js/main.js
+++ js/main.js
@@ -27,3 +27,32 @@ window.addEventListener('scroll', () =>{
     navbar.classList.remove('shadow', 'py-2')
   }
 })
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbysHHEXyyLzX3B_KwMqWIAoRfNNPlkOVXN36OyDaNeqUI7LItntLW5N16dXO_T2auyDEA/exec'
+const form = document.forms['web-contact-from']
+
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
+      
+form.addEventListener('submit', e => {
+  e.preventDefault()
+//   ketika tombol  submit diklik
+// tampilkan  tombol loading, hilangkan tombol kirim
+btnLoading.classList.toggle('d-none');
+btnKirim.classList.toggle('d-none');
+
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        // tampilkan  tombol kirim, hilangkan tombol loading
+        btnLoading.classList.toggle('d-none');
+        btnKirim.classList.toggle('d-none');
+        // tampilkan alert
+        myAlert.classList.toggle('d-none');
+        // reset form
+        form.reset();
+        console.log('Success!', response)
+    })
+    .catch(error => console.error('Error!', error.message))
+})
